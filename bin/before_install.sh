55fff19e83d374a7f428e0e5487c8f0bd2be56ac
d15y <104956837+d15y@users.noreply.github.com>
2022-12-13 16:08:58 -0800

    Update before_install.sh

--- bin/before_install.sh
+++ bin/before_install.sh
@@ -5,7 +5,17 @@ echo "START before_install"
 
 apt -y update
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# export 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 apt -y install nc
+
+apt install nginx -y
 
 curl –sL https://deb.nodesource.com/setup_14.x | sudo bash -
 apt -y install nodejs
