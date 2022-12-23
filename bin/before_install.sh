6310aa14a94e7e988a81b4702b413c7f5efc9dbc
d15y <104956837+d15y@users.noreply.github.com>
2022-12-23 09:55:32 -0800

    Update before_install.sh

--- bin/before_install.sh
+++ bin/before_install.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "START before_install"
 
-apt -y update
+sudo apt -y update
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
@@ -13,17 +13,17 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 14.15.4
-nvm use 14.15.4
+sudo nvm install 14.15.4
+sudo nvm use 14.15.4
 
-apt -y install netcat
+sudo apt -y install netcat
 
 #apt install nginx -y
 
 curl –sL https://deb.nodesource.com/setup_14.x | sudo bash -
-apt -y install nodejs
+sudo apt -y install nodejs
 
-npm install -g pm2
-pm2 update
+sudo npm install pm2 -g
+sudo pm2 update
 
 echo "END before_install"
